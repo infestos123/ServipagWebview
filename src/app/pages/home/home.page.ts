886042b80a6891a639dab8db6ea09d2e00838498
Daniel Castro <castrodaniel.m@gmail.com>
2020-06-12 18:02:55 -0400

    Se agrega estilo y estructura al page home

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,6 +8,9 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private router: Router) { }
 
+  goToContact() {
+    this.router.navigate(['/contact']);
+  }
 }
--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -18,7 +18,7 @@ export class LoginPage implements OnInit {
 
   signIn() {
     if (this.user === 'daniel' && this.password === '123') {
-      this.router.navigate(['/contact']);
+      this.router.navigate(['/home']);
     }
   }
 }
