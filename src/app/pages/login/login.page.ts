0d84fe00e0c5edf9baab4240df59fa9ababc9d9b
Daniel Castro <castrodaniel.m@gmail.com>
2020-06-12 01:13:00 -0400

    Modificaciones en el diseño de paginas

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  user: string;
+  password: string;
+
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
+  signIn() {
+    if (this.user === 'daniel' && this.password === '123') {
+      this.router.navigate(['/home']);
+    }
+  }
 }
