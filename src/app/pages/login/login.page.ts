35c5a78f73c21ee01bacbdbe5a294473eec1e4d2
Daniel Castro <castrodaniel.m@gmail.com>
2020-06-18 15:29:19 -0400

    Se agregan funciones del local storage para recordar contrasena

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,6 +1,9 @@
+import { ILogin } from './../../../@core/models/login.model';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Plugins } from '@capacitor/core';
+import { Observable } from 'rxjs';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
@@ -8,25 +11,61 @@ import { Plugins } from '@capacitor/core';
 })
 export class LoginPage implements OnInit {
 
+  recordPassword = false;
   user: string;
   password: string;
 
   constructor(private router: Router) { }
 
   ngOnInit() {
+    this.setDataFields();
+  }
+
+  async setDataToLocalStorage() {
+    await Plugins.Storage.set({
+      key: '1',
+      value: JSON.stringify({ rut: '145678900', password: '123456' })
+    });
+  }
+
+  async getDataFromLocalStorage() {
+    const { value } = await Plugins.Storage.get({ key: '1' });
+    return JSON.parse(value);
+  }
+
+  setDataFields() {
+    this.getDataFromLocalStorage().then(data => {
+
+      if (data === null) {
+        this.user = '';
+        this.password = '';
+      } else {
+        this.user = data.rut;
+        this.password = data.password;
+        this.recordPassword = true;
+      }
+    });
   }
 
   signIn() {
-    if (this.user === 'daniel' && this.password === '123') {
+    if (this.user === 'daniel' && this.password === '123' ||
+      this.user === '145678900' && this.password === '123456') {
       const rut = '145678900';
       const pass = '123456';
 
-      Plugins.Browser.open({ url: 'https://ww5.servipag.com/pagoenlinea/PA_LoginServipag/LoginApp?USER_NAME=' +
-                             rut + '&PASSWORD=' + pass, windowName: '_blank' });
+      if (this.recordPassword) {
+        this.setDataToLocalStorage();
+      }
+
+      Plugins.Browser.open({
+        url: 'https://ww5.servipag.com/pagoenlinea/PA_LoginServipag/LoginApp?USER_NAME=' +
+          rut + '&PASSWORD=' + pass, windowName: '_blank'
+      });
+
     }
   }
 
-  openUrlServipag(){
+  openUrlServipag() {
     Plugins.Browser.open({ url: 'https://www.servipag.com/mobile', windowName: '_self' });
   }
 }
